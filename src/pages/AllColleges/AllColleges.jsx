82a6c6e22c4baabb5fd49fadc38fcf7c91ad933d
Raqibur-Rahman <raqibur.edu@gmail.com>
2023-07-24 14:05:53 +0600

    search functionality added

--- src/pages/AllColleges/AllColleges.jsx
+++ src/pages/AllColleges/AllColleges.jsx
@@ -1,32 +1,64 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import UniversityCard from "../Shared/UniversityCard/UniversityCard";
 
 const AllColleges = () => {
     const [colleges, setColleges] = useState([]);
+    const [searchResults, setSearchResults] = useState([]);
+    const [searchQuery, setSearchQuery] = useState("");
 
     useEffect(() => {
-        fetch('http://localhost:5000/colleges')
-            .then(res => res.json())
-            .then(data => {
+        fetch("http://localhost:5000/colleges")
+            .then((res) => res.json())
+            .then((data) => {
                 setColleges(data);
-            })
-    }, [])
+            });
+    }, []);
 
+    const handleSearchInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
 
+    const handleSearch = () => {
+        const filteredColleges = colleges.filter((college) =>
+            college.collegeName.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setSearchResults(filteredColleges);
+    };
+
+    useEffect(() => {
+        // Trigger search on searchQuery change
+        handleSearch();
+    }, [searchQuery, colleges]);
 
     return (
-        <div>
-           
-            <div className="grid md:grid-cols-3 gap-4">
-                {
-                    colleges.map(item => <UniversityCard
-                        key={item._id}
-                        item={item}
-                    ></UniversityCard>)
-                }
+        <div className="flex flex-col items-center">
+            {/* Search bar */}
+            <div className="mb-4">
+                <label htmlFor="search" className="text-xl font-semibold">
+                    Search for a college name:
+                </label>
+                <input
+                    type="text"
+                    id="search"
+                    value={searchQuery}
+                    onChange={handleSearchInputChange}
+                    placeholder="Enter college name..."
+                    className="border rounded-lg py-1 px-2"
+                />
             </div>
 
+            {/* Colleges grid */}
+            <div className="grid md:grid-cols-3 gap-4">
+                {searchResults.length === 0 ? (
+                    <div className="flex justify-center items-center w-full h-96">
+                        <p className="text-3xl text-red-500 font-semibold animate-bounce">No results found.</p>
+                    </div>
+                ) : (
+                    searchResults.map((item) => (
+                        <UniversityCard key={item._id} item={item}></UniversityCard>
+                    ))
+                )}
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,15 +1,47 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-
+import Swal from "sweetalert2";
 
 const Navbar = () => {
-    const navOptions = (
-        <>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/colleges">Colleges</Link></li>
-            <li><Link to="/admission">Admission</Link></li>
-            <li><Link to="/mycollege">My College</Link></li>
-        </>
-    );
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleSearchInputChange = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+
+
+    const handleSearch = () => {
+        Swal.fire({
+            icon: 'info',
+            title: 'Perform Search on All Colleges Page',
+            text: 'For faster and more accurate results, please go to the "All Colleges" page and use the search box there.',
+            confirmButtonText: 'Go to All Colleges Page',
+            showCancelButton: true,
+            cancelButtonText: 'Close',
+            allowOutsideClick: true,
+            allowEscapeKey: true,
+            allowEnterKey: true,
+            stopKeydownPropagation: false,
+            focusCancel: true,
+            focusConfirm: false,
+            showCloseButton: false,
+            customClass: {
+                content: 'text-base-content',
+                footer: 'text-base-content',
+            },
+            willOpen: () => {
+                // Handle the link click manually to navigate to "/colleges"
+                const link = Swal.getPopup().querySelector('.swal2-confirm');
+                if (link) {
+                    link.addEventListener('click', () => {
+                        // Navigate to "/colleges" when the link is clicked
+                        window.location.href = '/colleges';
+                    });
+                }
+            },
+        });
+    };
 
     return (
         <>
@@ -17,29 +49,80 @@ const Navbar = () => {
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16"
+                                />
+                            </svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            {navOptions}
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/colleges">Colleges</Link>
+                            </li>
+                            <li>
+                                <Link to="/admission">Admission</Link>
+                            </li>
+                            <li>
+                                <Link to="/mycollege">My College</Link>
+                            </li>
                         </ul>
                     </div>
                     <Link to="/" className="font-extrabold text-3xl">
                         AcademiGate
                     </Link>
-
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
-                        {navOptions}
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/colleges">Colleges</Link>
+                        </li>
+                        <li>
+                            <Link to="/admission">Admission</Link>
+                        </li>
+                        <li>
+                            <Link to="/mycollege">My College</Link>
+                        </li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className="flex items-center space-x-4">
+                        {/* Search box */}
+                        <input
+                            type="text"
+                            placeholder="Search for a college"
+                            className="border rounded-lg py-1 px-2"
+                            value={searchQuery}
+                            onChange={handleSearchInputChange}
+                        />
+                        <button className="btn btn-primary" onClick={handleSearch}>
+                            Search
+                        </button>
+                        {/* Login button */}
+                        <Link to="/login">
+                            <button className="btn btn-primary">Login</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-
         </>
-
     );
 };
 
